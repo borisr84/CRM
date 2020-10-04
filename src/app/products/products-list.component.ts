eacd116dabb1dbc58297b14ec3cd7e359e738f7a
Boris <borisr84azdev@hotmail.com>
2020-10-04 11:07:01 +0200

    Add list filtering

--- src/app/products/products-list.component.ts
+++ src/app/products/products-list.component.ts
@@ -9,17 +9,45 @@ import { ProductService } from './product.service'
 })
 export class ProductsListComponent implements OnInit {
   products : IProduct[] = [];
+  filteredProducts : IProduct[];
   errorMsg : string;
+  isFilterByStartsWith : boolean = true;
+  //filterBy : string;
 
   constructor(private prodService : ProductService) { }
 
   ngOnInit(): void {
     this.prodService.getProducts().subscribe(
       {
-        next: prods => this.products = prods,
+        next: prods => {
+          this.products = prods;
+          this.filteredProducts = prods;
+        },
         error: err => this.errorMsg = err
       }
     )
+  }
+
+  // filterByChangeFn(value : string) {
+  //   this.filteredProducts = this.products.filter((p : IProduct) => p.name.toLowerCase().includes(value.toLowerCase()));
+  // }
+
+  private _filterBy : string;
+  get filterBy() : string{
+    return this._filterBy;
+  }
+
+  set filterBy(value: string){
+    this._filterBy = value;
+
+    this.filteredProducts = this.products.filter((p : IProduct) => this.isFilterByStartsWith ? 
+      p.name.toLowerCase().startsWith(value.toLowerCase()) : 
+      p.name.toLowerCase().includes(value.toLowerCase()));
+  }
+
+  onFilterByToggle() {
+    this.isFilterByStartsWith = !this.isFilterByStartsWith;
+    this.filterBy = this._filterBy;
   }
 
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,13 +4,14 @@ import { ProductsListComponent } from './products-list.component';
 import { RouterModule } from '@angular/router';
 import { QuantityPipe } from './quantity.pipe';
 import { ProductDetailsComponent } from './product-details.component';
-
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ProductsListComponent, QuantityPipe, ProductDetailsComponent],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(
       [
         {path: 'products', component: ProductsListComponent},
